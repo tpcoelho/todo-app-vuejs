abde766fbd251e64d590d2c3d238f4cb3cdf5f88
Tiago Coelho <tiagocoelho33@gmail.com>
2019-06-09 22:53:18 -0300

    fix server

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
-console.log("Server started...");
+console.log("Server started..."+port);
